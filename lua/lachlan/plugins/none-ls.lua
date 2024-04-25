cd63f3106cb9cddbdd934cb40380b319d80f1503
Lachlan Garrahy <lachikent@gmail.com>
2024-04-25 18:07:07 +1000

    comments + pylint

--- lua/lachlan/plugins/none-ls.lua
+++ lua/lachlan/plugins/none-ls.lua
@@ -8,9 +8,19 @@ return {
 
     null_ls.setup({
       sources = {
+        -- Lua
         null_ls.builtins.formatting.stylua,
+
+        -- Python
+        -- null_ls.builtins.diagnostics.pylint.with({
+        --  diagnostics_postprocess = function(diagnostic)
+        --     diagnostic.code = diagnostic.message_id
+        --   end,
+        -- }),
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
+
+        -- JavaScript + TypeScript
         null_ls.builtins.formatting.prettier,
         require("none-ls.diagnostics.eslint"),
       },

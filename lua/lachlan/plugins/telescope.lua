9e314f3f7dfe5801f8b6a01c323957dc1ef87396
Lachlan Garrahy <lachikent@gmail.com>
2024-04-30 01:30:49 +1000

    added git files

--- lua/lachlan/plugins/telescope.lua
+++ lua/lachlan/plugins/telescope.lua
@@ -5,7 +5,8 @@ return {
     config = function()
       -- telescope rebinds
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
   },

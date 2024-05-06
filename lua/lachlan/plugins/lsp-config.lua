0f74a7b6c765bcc870503ddb2a2b42f2bc670102
Lachlan Garrahy <lachikent@gmail.com>
2024-05-07 01:57:31 +1000

    vimttest and rename

--- lua/lachlan/plugins/lsp-config.lua
+++ lua/lachlan/plugins/lsp-config.lua
@@ -37,13 +37,13 @@ return {
 				capabilities = capabilities,
 			})
       lspconfig.gopls.setup({
-        on_attach = on_attach,
         capabilities = capabilities,
       })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 		end,
 	},
 }

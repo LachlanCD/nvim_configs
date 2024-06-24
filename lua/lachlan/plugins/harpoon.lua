2bcb9067ee491839f4617b783757cf2d7f6e9a18
Lachlan Garrahy <lachikent@gmail.com>
2024-06-24 15:42:38 +1000

    keybinds

--- lua/lachlan/plugins/harpoon.lua
+++ lua/lachlan/plugins/harpoon.lua
@@ -32,7 +32,7 @@ return {
 				:find()
 		end
 
-		vim.keymap.set("n", "<C-e>", function()
+		vim.keymap.set("n", "<leader>ha", function()
 			toggle_telescope(harpoon:list())
 		end, { desc = "Open harpoon window" })
 
